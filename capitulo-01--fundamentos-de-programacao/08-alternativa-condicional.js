4cde381a8a9c56fe5008fe5d2f4066c928aa43fb
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-11-13 01:42:39 -0300

    Update 08-alternativa-condicional.js

--- capitulo-01--fundamentos-de-programacao/08-alternativa-condicional.js
+++ capitulo-01--fundamentos-de-programacao/08-alternativa-condicional.js
@@ -12,9 +12,58 @@ procedure RetirarAzulComMedo() {
 }
 
 // Exercícío 3
+procedure RetirarVermelhaComMedo() {
+  if (haPedras(Vermelho)) {
+    Retirar(Vermelho)
+  }
+}
+
 // Exercícío 4
+procedure CompletarCelula() {
+  if (haPedras(Preto)) {
+    Colocar(Vermelho)
+    Colocar(Azul)
+    Colocar(Verde)
+  }
+}
+
 // Exercícío 5
+program{
+  if (podeMover(Leste)){
+    Mover(Leste)
+  }
+}
+
 // Exercícío 6
+program {
+  if((nroPedras(Preto) < 5)){
+    Colocar(Preto)
+    }
+}
+
 // Exercícío 7
+procedure GarantirUmaPedraVerde(){
+  if (not haPedras(Verde)){
+    Colocar(Verde)
+  }
+}
+
 // Exercícío 8
+procedure MoverComoSeja() {
+    if (podeMover(Oeste)) {
+        Mover(Oeste)
+    } else {
+        Mover(Norte)
+    }
+}
+
 // Exercícío 9
+procedure AcenderOuApagarLuz(){
+  if(haPedras (Verde)){
+    Retirar (Verde)
+    Colocar(Preto)
+ }else {
+   Retirar (Preto)
+   Colocar(Verde)
+    }
+}

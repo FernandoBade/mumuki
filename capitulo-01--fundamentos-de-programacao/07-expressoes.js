69b7aa900bec4697f9f399842b99ebaefc061c42
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-11-13 01:10:52 -0300

    Update 07-expressoes.js

--- capitulo-01--fundamentos-de-programacao/07-expressoes.js
+++ capitulo-01--fundamentos-de-programacao/07-expressoes.js
@@ -1,12 +1,70 @@
 // 7. Expressões
 // Exercícío 1
+program {
+  ColocarN (3+2, Preto)
+}
+
 // Exercícío 2
+procedure ColocarSoma(x, y){
+  ColocarN(x+y, Vermelho)
+}
+
 // Exercícío 3
+procedure ContarGente(onibus, carros, bicicletas){
+  ColocarN (onibus * 40 + carros * 4 + bicicletas, Verde)
+}
+
 // Exercícío 4
+procedure MoverComoSalmao(direcao) {
+  Mover(oposto(direcao))
+}
+
 // Exercícío 5
+procedure CaminharDesconfiado(passos){
+  MoverN(passos div 2, Leste)
+}
+
 // Exercícío 6
+procedure ColocarAo(direcao, cor){
+  Mover(direcao)
+  Colocar(cor)
+  Mover(oposto(direcao))
+}
+
 // Exercícío 7
+procedure Linha(direcao, cor, comprimento) {
+  repeat(comprimento) {
+    Colocar(cor)
+    MoverN(1, direcao)
+  } repeat(comprimento){
+    Mover(oposto(direcao))
+  }
+}
+
 // Exercícío 8
+procedure Ele(direcao){
+  Linha(direcao, Azul, 3)
+  Linha(seguinte(direcao), Azul, 3)
+}
+
 // Exercícío 9
+procedure Seta(direcao){
+  MoverN(1, direcao)
+  ColocarN(1, Vermelho)
+  MoverN(1, seguinte(direcao))
+  Mover(oposto(direcao))
+  ColocarN(1, Vermelho)
+  MoverN(2, previo(direcao))
+  ColocarN(1, Vermelho)
+  MoverN(1, seguinte(direcao))
+}
+
 // Exercícío 10
+procedure CopiarVerdesEmVermelhas(){
+  ColocarN(nroPedras(Verde), Vermelho)
+}
+
 // Exercícío 11
+procedure RetirarTodas(cor){
+  RetirarN(nroPedras(cor), cor)
+}

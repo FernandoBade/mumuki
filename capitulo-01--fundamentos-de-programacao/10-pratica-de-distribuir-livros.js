046a8637ee5d7089cf600117559cfdc2e153bc44
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-11-14 21:09:42 -0300

    Update 10-pratica-de-distribuir-livros.js

--- capitulo-01--fundamentos-de-programacao/10-pratica-de-distribuir-livros.js
+++ capitulo-01--fundamentos-de-programacao/10-pratica-de-distribuir-livros.js
@@ -1,10 +1,66 @@
 // 10. Prática de Distribuir Livros
 // Exercícío 1
+function corEstudantes(){
+  return (Azul)
+}
+
+function quantidadeEstudantes(){
+  return (nroPedras(corEstudantes()))
+}
+
 // Exercícío 2
+function corLivros(){
+  return (Preto)
+}
+  
+function quantidadeLivros(){
+  return(nroPedras(corLivros()))
+ }
+
 // Exercícío 3
+procedure EntregarLivros(quantidadeLivros){
+
+  ColocarN(quantidadeLivros, corLivros())
+}
+
 // Exercícío 4
+procedure EntregaroQueDer(quantidadeLivros){
+  if (quantidadeLivros <= 50){
+    EntregarLivros(quantidadeLivros)
+} if (quantidadeLivros > 50){
+  EntregarLivros(50)
+}
+}
+
 // Exercícío 5
+function corSobrantes(){
+  return (Verde)
+}
+
+function corFaltantes(){
+  return (Vermelho)
+}
+
+procedure TransmitirSituacao(){
+  if (quantidadeLivros() > quantidadeEstudantes()){
+    ColocarN((quantidadeLivros() - quantidadeEstudantes()),  corSobrantes())
+} if (quantidadeLivros() < quantidadeEstudantes()){
+    ColocarN((quantidadeEstudantes() - quantidadeLivros()), corFaltantes())
+}  
+}
+
 // Exercícío 6
+procedure FazerTrabalhoNaEscola (){
+  if (quantidadeLivros() > quantidadeEstudantes()){
+     TransmitirSituacao()
+} if ((quantidadeEstudantes() - quantidadeLivros())<= 50){
+     EntregaroQueDer(quantidadeEstudantes() - quantidadeLivros())
+} if ((quantidadeEstudantes() - quantidadeLivros())> 50){
+     EntregaroQueDer(quantidadeEstudantes() - quantidadeLivros())
+       TransmitirSituacao()
+}
+}
+
 // Exercícío 7
 // Exercícío 8
 // Exercícío 9

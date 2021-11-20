8eb4238e718a98ba26526623ee10eeee28279975
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-11-20 04:53:15 -0300

    Update 12-variaveis.js

--- capitulo-01--fundamentos-de-programacao/12-variaveis.js
+++ capitulo-01--fundamentos-de-programacao/12-variaveis.js
@@ -1,9 +1,37 @@
 // 12. Variáveis
 // Exercícío 1
+procedure CopiarVerdesAoNorte() {
+  Mover(Norte)
+  ColocarN(pedrasVerdesAoSul(), Verde)
+}
+
+function pedrasVerdesAoSul() {
+  Mover(Sul)
+  return (nroPedras(Verde))
+}
+
 // Exercícío 2
+procedure CopiarVerdesAoNorte() {
+  quantidadePedrasNaCelulaInicial := nroPedras(Verde)
+  Mover(Norte)
+  ColocarN(quantidadePedrasNaCelulaInicial, Verde)
+}
+
 // Exercícío 3
+function corLouca() {
+  corzinha := Verde //resposta: Verde
+  return (corzinha)
+}
+
 // Exercícío 4
+Sul
+
 // Exercícío 5
+function boomBoomKid() {
+  return (umaVariavelQueNaoExiste)
+} //[2:10]: A variável "umaVariavelQueNaoExiste" não está definida.
+
+
 // Exercícío 6
 // Exercícío 7
 // Exercícío 8

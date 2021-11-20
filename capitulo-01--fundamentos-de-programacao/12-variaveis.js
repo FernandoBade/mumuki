c7b560481c1d73075665a1d1998b46f1a9fd5929
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-11-20 16:48:25 -0300

    Update 12-variaveis.js

--- capitulo-01--fundamentos-de-programacao/12-variaveis.js
+++ capitulo-01--fundamentos-de-programacao/12-variaveis.js
@@ -33,9 +33,65 @@ function boomBoomKid() {
 
 
 // Exercícío 6
+function quemGanhou(){
+  if (golsVasco() < golsFlamengo()){
+    vencedor := Vermelho
+  } if (golsFlamengo() < golsVasco()){
+    vencedor := Preto
+  }return (vencedor)
+}
+
 // Exercícío 7
+function maximo (x, y){
+  if (x > y){
+    resultado := x
+  } if (y > x){
+    resultado := y
+  } return (resultado)
+}
+
 // Exercícío 8
+function maximaQuantidadeDePedras(){
+  atual := nroPedrasTotal ()
+  vizinha := 0
+  if (nroPedrasTotal () >= 0){
+    atual := nroPedrasTotal ()
+  } Mover(Leste)
+  if (nroPedrasTotal () >= 0){
+    vizinha := nroPedrasTotal()
+  }return (maximo(atual, vizinha))
+} 
+
 // Exercícío 9
+55
+
 // Exercícío 10
+function numero(){
+  primeiroNumero := algarismo() * 100
+  Mover(Leste)
+  segundoNumero := primeiroNumero + algarismo() * 10
+  Mover(Leste)
+  terceiroNumero := segundoNumero + algarismo()
+  return(terceiroNumero)
+}
+
 // Exercícío 11
+procedure ColherHorta() {
+  tomatesColhidos := 0
+  repeat (3) {
+    MoverN(1, Leste)
+    tomatesColhidos := tomatesColhidos + quantidadeTomates()
+    ColherPlanta()
+  } MoverN(3, Oeste)
+  DeixarTomates(tomatesColhidos)
+}
+
 // Exercícío 12
+function quantoFaltaAte(direcao) {
+  distancia := 0
+
+  while (podeMover(direcao)) {
+    Mover(direcao)
+    distancia := distancia + 1
+  } return (distancia)
+}

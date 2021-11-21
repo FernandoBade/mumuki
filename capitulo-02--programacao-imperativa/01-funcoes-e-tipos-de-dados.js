4b0c8a0f487279c7e5debc65fb95184e77f425d2
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-11-21 15:17:44 -0300

    Update 01-funcoes-e-tipos-de-dados.js

--- capitulo-02--programacao-imperativa/01-funcoes-e-tipos-de-dados.js
+++ capitulo-02--programacao-imperativa/01-funcoes-e-tipos-de-dados.js
@@ -22,11 +22,39 @@ function anterior(numero){
    }
    
 // Exercícío 3
+4 + 5
+absoluto(-20)
+funcaoLouca(1, 2, 3)
+
 // Exercícío 4
+function ePar(i){
+if (i%2 === 0){
+   return true
+}
+}
+
 // Exercícío 5
+5 + 6 
+5 === 6 
+8 > 6 
+!true
+false / true
+
 // Exercícío 6
+function dobroDoAbsoluto(numero) {
+   return 2 * Math.abs(numero);
+}
+
 // Exercícío 7
+function extrair(saldo, quantidade) {
+  return Math.max(saldo - quantidade, 0);
+}
+
 // Exercícío 8
+function quantoCarrega(atual, carregamento){
+  return Math.min(25 - atual, carregamento)
+}
+
 // Exercícío 9
 // Exercícío 10
 // Exercícío 11

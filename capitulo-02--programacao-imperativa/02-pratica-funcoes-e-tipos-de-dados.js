4caa5ba73c55a28f94c49191d6e031e5d18581c1
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-12-05 18:32:38 -0300

    Update 02-pratica-funcoes-e-tipos-de-dados.js

--- capitulo-02--programacao-imperativa/02-pratica-funcoes-e-tipos-de-dados.js
+++ capitulo-02--programacao-imperativa/02-pratica-funcoes-e-tipos-de-dados.js
@@ -10,10 +10,64 @@ function meConvem (polegadas, memoria){
 }
 
 // Exercícío 3
+function perimetroTriangulo(x, y, z){
+  return x + y + z
+}
+
+function areaTriangulo (x, y){
+  return x * y /2
+}
+
 // Exercícío 4
+function perimetroQuadrado (lado){
+  return lado * 4
+}
+
+function areaQuadrado (lado){
+  return lado * lado
+}
+
 // Exercícío 5
+function estaAfinado(frequencia){
+  return frequencia === 440
+}
+
 // Exercícío 6
+function estaPerto(frequencia){
+  return frequencia <= 443 && frequencia >= 437 && frequencia !== 440
+}
+
 // Exercícío 7
+function escreverCartao(pronome, nome, sobrenome){
+  return pronome + " " + nome + " " + sobrenome
+}
+
 // Exercícío 8
+function escreverCartao(pronome, nome, sobrenome, tipo){
+  if (tipo){
+    return pronome + " " + sobrenome 
+  } else {
+    return pronome + " " + nome + " " + sobrenome 
+  }
+}
+
 // Exercícío 9
+function escreverCartao(pronome, nome, sobrenome, tipo){
+  if (tipo){
+    return pronome + " " + sobrenome 
+  } else {
+    return pronome + " " + nome + " " + sobrenome 
+  }
+}
+
+function escreverCartaoOptimo (pronome, nome, sobrenome){
+return escreverCartao(pronome, nome, sobrenome,(longitude(nome + sobrenome)>=15))
+}
+
 // Exercícío 10
+function decisaoComMoeda(moeda, comida1, comida2){
+let cara = "cara"
+let coroa = "coroa"
+  
+  return moeda === cara && comida1 || moeda === coroa && comida2
+}

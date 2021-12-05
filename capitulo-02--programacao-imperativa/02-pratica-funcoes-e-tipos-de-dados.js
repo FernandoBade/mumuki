7dca11c737bed636977dda2a6984b9395d4dae0a
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-12-05 16:04:32 -0300

    Update 02-pratica-funcoes-e-tipos-de-dados.js

--- capitulo-02--programacao-imperativa/02-pratica-funcoes-e-tipos-de-dados.js
+++ capitulo-02--programacao-imperativa/02-pratica-funcoes-e-tipos-de-dados.js
@@ -1,6 +1,14 @@
 // 2.Prática Funções e Tipos de Dados
 // Exercícío 1
+function quantoCusta (polegadas, memoria){
+  return (polegadas * 60) + (memoria * 200) + 1000
+}
+
 // Exercícío 2
+function meConvem (polegadas, memoria){
+  return polegadas >= 32 && memoria >= 8 && quantoCusta(polegadas, memoria) < 6000
+}
+
 // Exercícío 3
 // Exercícío 4
 // Exercícío 5

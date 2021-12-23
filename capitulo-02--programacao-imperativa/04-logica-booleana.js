854dc3e94004cada5bed8350a75bef4a98ff28f0
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-12-23 02:18:13 -0300

    Update 04-logica-booleana.js

--- capitulo-02--programacao-imperativa/04-logica-booleana.js
+++ capitulo-02--programacao-imperativa/04-logica-booleana.js
@@ -1,9 +1,49 @@
 // 4.Lógica booleana
 // Exercícío 1
+let camaCima = true;
+
+function brincarNoHospital (){
+  camaCima = !camaCima
+}
+
 // Exercícío 2
+function eMaiorDeIdade(numero){
+  return numero >= 18
+}
+
+function eMenorDeIdade(numero){
+  return !eMaiorDeIdade(numero)
+}
+
 // Exercícío 3
+function ePeripatetico(x, y, z){
+  let profissao = "filósofo"
+  let nacionalidade = "grego"
+  let anda = 2
+  
+  return x === profissao && y === nacionalidade && z > 2
+}
+
 // Exercícío 4
+   ePeripatetico("filosofo", "grego", 5)
+=> true
+   ePeripatetico("atleta", "brasileiro", 10)
+=> false
+   ePeripatetico("filosofo", "brasileiro", 3)
+=> false
+   ePeripatetico("professor", "canadense", 1)
+=> false
+   
 // Exercícío 5
+   ganhou(true, 25)
+=> true
+   ganhou(false, 30)
+=> true
+   ganhou(false, 20)
+=> false
+   ganhou(true, 31)
+=> true
+
 // Exercícío 6
 // Exercícío 7
 // Exercícío 8
